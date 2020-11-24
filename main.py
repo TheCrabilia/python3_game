4b623b0042742ecaa610510bf1aeca1c7f2f9643
TheCrabilia <ilja.spirts@outlook.com>
2020-11-24 17:26:50 +0200

    Implemented obstacle object spawn, player and obstacle collision detection. Code refactoring.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame as pg
+import random
 from colors import *
 
 
@@ -6,14 +7,18 @@ class GameWindow:
     """
     Game window class. Contains main game loop.
     """
+
     def __init__(self, size: tuple):
         self.width, self.height = size
         self.screen = pg.display.set_mode((self.width, self.height))
 
-        # Objects that are drawn on the screen
-        self.player = Player(position=(200, self.height - 20), radius=10, color=RED)
-        self.score_widget = Score(position=(700, 20), font_size=32)
-        self.obstacles = [Obstacle(position=(50, 50), dimensions=(70, 70), color=GREEN)]
+        self.player = Player(position=(100, self.height - 20), radius=10, color=RED)
+        self.score_widget = Score(position=(700, 20), font_size=32, bg_color=GRAY)
+
+        self._obstacles = []
+        self.obstacle_spawn_delay = 60
+
+        self.game_over = False
 
     def main_loop(self):
         """
@@ -26,8 +31,12 @@ class GameWindow:
         # Initialize pygame
         pg.init()
 
+        # Set game screen caption
+        pg.display.set_caption("Simple game")
+
         finished = False
 
+        # Game loop
         while not finished:
             clock.tick(fps)
 
@@ -35,15 +44,29 @@ class GameWindow:
                 if event.type == pg.QUIT:
                     finished = True
 
-            keys = pg.key.get_pressed()
+            self.screen.fill(GRAY)
 
-            if not self.player.is_jump:
-                if keys[pg.K_SPACE]:
-                    self.player.is_jump = True
+            if not self.game_over:
+                keys = pg.key.get_pressed()
+                if not self.player.get_jump_status():  # True if method returns False (player does not jump)
+                    if keys[pg.K_SPACE] or keys[pg.K_UP]:
+                        self.player.set_jump_status(True)
+                else:
+                    self.player.jump()
+
+                # Destroy obstacle if it is out of the screen
+                self.destroy_obstacle()
+
+                # Move obstacles on the screen
+                for obstacle in self._obstacles:
+                    obstacle.move()
+
+                self.detect_collision()
+                self.spawn_new_obstacle()
             else:
-                self.player.jump()
+                Text(text="Game Over", position=(self.width / 2 - 150, self.height / 2 - 50),
+                     font_size=50, bg_color=GRAY).draw(self.screen)
 
-            self.screen.fill(WHITE)
             self.update_objects()
             pg.display.update()
 
@@ -51,42 +74,80 @@ class GameWindow:
         """
         Updates all objects on the screen.
         """
-        for obstacle in self.obstacles:
+        # Draw obstacles on the screen
+        for obstacle in self._obstacles:
             obstacle.draw(self.screen)
 
+        # Draw player on the screen
         self.player.draw(self.screen)
+        # Draw score widget on the screen
         self.score_widget.draw(self.screen)
+
+    def spawn_new_obstacle(self):
+        if self.obstacle_spawn_delay == 60:
+            dimensions = [(20, 60), (20, 20)]
+            random_dimensions = random.choices(dimensions, weights=(70, 45), k=1)
+            # Create new obstacle
+            new_obstacle = Obstacle(position=(self.width + 20, self.height - 70),
+                                    dimensions=random_dimensions[0], color=BLACK)
+            self._obstacles.append(new_obstacle)
+            self.obstacle_spawn_delay = 0
+        else:
+            self.obstacle_spawn_delay += 1
+
+    def destroy_obstacle(self):
+        if len(self._obstacles) != 0:
+            if self._obstacles[0].get_x() == 0:
+                self._obstacles.pop(0)
+
+    def detect_collision(self):
+        player_x, player_y, player_radius = self.player.get_metrics()
+        for obstacle in self._obstacles:
+            obstacle_x, obstacle_y, obstacle_width, obstacle_height = obstacle.get_metrics()
+            if obstacle_y <= (player_y + player_radius) <= (obstacle_y + obstacle_height):
+                if obstacle_x <= (player_x + player_radius) <= (obstacle_x + obstacle_width):
+                    self.game_over = True
 
 
 class Player:
     """
     Player class. Draws player on the main game window, realizes player mechanic - jump.
     """
-    # Jump status class variable (available outside the class)
-    is_jump = False
-
     def __init__(self, position: tuple, radius: int, color: tuple, speed: int = 10):
         self.x, self.y = position
         self.radius = radius
         self.speed = speed
         self.color = color
 
-        # Jump mechanic variable
-        self._jump_count = 10
+        # Jump mechanic variables
+        self._jump_count = 8
+        self.is_jump = False
 
     def draw(self, screen):
         pg.draw.circle(screen, self.color, (self.x, self.y), self.radius)
 
     def jump(self):
-        if self._jump_count >= -10:
+        if self._jump_count >= -8:
             if self._jump_count < 0:
                 self.y += (self._jump_count ** 2) / 2
             else:
                 self.y -= (self._jump_count ** 2) / 2
             self._jump_count -= 1
         else:
+            self.set_jump_status(False)
+            self._jump_count = 8
+
+    def set_jump_status(self, status):
+        if status:
+            self.is_jump = True
+        else:
             self.is_jump = False
-            self._jump_count = 10
+
+    def get_jump_status(self):
+        return self.is_jump
+
+    def get_metrics(self):
+        return self.x, self.y, self.radius
 
 
 class Obstacle:
@@ -95,8 +156,19 @@ class Obstacle:
         self.width, self.height = dimensions
         self.color = color
 
+        self._speed = 10
+
     def draw(self, screen):
         pg.draw.rect(screen, self.color, (self.x, self.y, self.width, self.height))
+
+    def move(self):
+        self.x -= self._speed
+
+    def get_x(self):
+        return self.x
+
+    def get_metrics(self):
+        return self.x, self.y, self.width, self.height
 
 
 class Text:
@@ -119,17 +191,21 @@ class Score(Text):
     """
     Game score class. Contains score incrementation method.
     """
+
     def __init__(self, position: tuple, font_size: int = 10, prefix: str = "Score: ",
                  fg_color: tuple = BLACK, bg_color: tuple = WHITE):
-        self.score = 0
+        self.points = 0
         self.prefix = prefix
-        self.text = self.prefix + str(self.score)
+        self.text = self.prefix + str(self.points)
         super().__init__(self.text, position, font_size, fg_color, bg_color)
 
     def increment_score(self):
-        self.score += 1
-        self.text = self.prefix + str(self.score)
+        self.points += 1
+        self.text = self.prefix + str(self.points)
         self.rendered_text = self.font.render(self.text, True, self.fg_color, self.bg_color)
+
+    def get_points(self):
+        return self.points
 
 
 def main():
